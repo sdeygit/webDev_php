6d7ddbbc239ce87c2a449bfe7892352e71897a47
sdeygit <sdey.jnu@gmail.com>
2025-11-14 14:04:09 +0600

    index.php file modified

--- c_18/index.php
+++ c_18/index.php
@@ -22,4 +22,7 @@
     echo $a, $b, $c . "<br>";   // echo can handle multiple arguement at a time before concatenation. echo is first.
     
     // print $a, $b, $c . "<br>";   // print can't handle multiple arguement
+    echo "<br>";
+
+    echo "File modified!"
 ?>
