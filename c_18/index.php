20bab0dd1c3b9a1760665f37536bdb59be3a537a
sdeygit <sdey.jnu@gmail.com>
2025-11-14 13:39:54 +0600

    index.php is added in c_18 folder

---
+++ c_18/index.php
@@ -0,0 +1,25 @@
+<?php
+    echo "Hello!";
+    echo "<br>"; 
+    echo "<br>"; 
+
+    echo "Variables :"; 
+    echo "<br>"; 
+
+    $a = 10;
+    $b = 20;
+    $c = $a + $b;
+
+    echo $c;
+    echo "<br>";
+
+    echo "The some of a and b = " . $c;     // 30
+    echo "<br>";
+
+    echo $a . "<br>" . $b;      // Will give result
+    echo "<br>";
+
+    echo $a, $b, $c . "<br>";   // echo can handle multiple arguement at a time before concatenation. echo is first.
+    
+    // print $a, $b, $c . "<br>";   // print can't handle multiple arguement
+?>
